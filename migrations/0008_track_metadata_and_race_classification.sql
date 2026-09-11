103285f12f55a907ed0fe40b30d17445073bebbd
mstpln <mattias.bjerstedt@gmail.com>
2026-09-11 09:00:55 +0200

    Keep race classification derived from factual source fields

--- migrations/0008_track_metadata_and_race_classification.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE tracks ADD COLUMN street_address TEXT;
-ALTER TABLE tracks ADD COLUMN postal_code TEXT;
-ALTER TABLE tracks ADD COLUMN website_url TEXT;
-
-ALTER TABLE races ADD COLUMN stl_class TEXT;
-ALTER TABLE races ADD COLUMN race_types_json TEXT;
-
-CREATE INDEX IF NOT EXISTS idx_races_track_stl_class
-  ON races(track_id, stl_class);
