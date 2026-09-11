5bbd899a17400bd6d2e9906627a76f4da9727327
mstpln <mattias.bjerstedt@gmail.com>
2026-09-11 22:51:29 +0200

    Expose relevant horse patterns in detail statistics

--- src/statistics/horses-complete.js
+++ src/statistics/horses-complete.js
@@ -5,6 +5,7 @@ import {
   normalizeHorseStatsFilters
 } from './horses.js';
 import { getHorseStartPointHistory, getHorseStartPointRanking } from './horse-start-points.js';
+import { getHorseRelevantPatterns } from './horse-patterns.js';
 
 export { getHorseFilterOptions, normalizeHorseStatsFilters };
 
@@ -24,11 +25,15 @@ export async function getHorseRankings(env, options = {}) {
 export async function getHorseDetailStatistics(env, horseId, options = {}) {
   const data = await getBaseHorseDetailStatistics(env, horseId, options);
   if (!data) return null;
-  const startPoints = await getHorseStartPointHistory(env, data.horseId, data.filters.asOfDate);
+  const [startPoints, patterns] = await Promise.all([
+    getHorseStartPointHistory(env, data.horseId, data.filters.asOfDate),
+    getHorseRelevantPatterns(env, data.horseId, data.filters.asOfDate)
+  ]);
   return {
     ...data,
     currentStartPoints: startPoints.current,
     startPointHistory: startPoints.history,
-    startPointsStatus: 'verified_official_life_statistics'
+    startPointsStatus: 'verified_official_life_statistics',
+    relevantPatterns: patterns
   };
 }
