f120939a291e5b8954d49c63cd9042093da87017
mstpln <mattias.bjerstedt@gmail.com>
2026-09-11 13:14:15 +0200

    feat: pass race scope through private statistics APIs

--- src/worker-settings.js
+++ src/worker-settings.js
@@ -1,6 +1,6 @@
 import worker from './worker-pwa.js';
 import { appAuthConfigured, clearAppSessionCookie, hasValidAppSession } from './app-auth.js';
-import { htmlResponse, redirectResponse, renderAppPage } from './app-page-aligned.js';
+import { htmlResponse, redirectResponse, renderAppPage } from './app-page-scope-polish.js';
 import { XLABS_SCRIPT_SELECTOR_VERSION } from './provider/xlabs-script.js';
 import { KENTAURAI_APP_VERSION, createFullDataExportResponse, getSettingsStatus, importAnalysisUpload } from './settings-data-display.js';
 import { getEnhancedGameHistoryDetail } from './routes/game-detail-display.js';
@@ -154,6 +154,7 @@ export default {
       try {
         const data = await getFilteredEntityStatBreakdowns(env, statBreakdownMatch[1], decodeURIComponent(statBreakdownMatch[2]), {
           year: url.searchParams.get('year'),
+          raceScope: url.searchParams.get('race_scope'),
           distanceStartMethod: url.searchParams.get('distance_start_method'),
           trackStartMethod: url.searchParams.get('track_start_method')
         });
