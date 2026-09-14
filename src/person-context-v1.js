b991e0e8f01be2b1643acea87dbadbc499742343
mstpln <mattias.bjerstedt@gmail.com>
2026-09-14 16:44:26 +0200

    Expand B5 person context module

--- src/person-context-v1.js
+++ src/person-context-v1.js
@@ -1,2 +1,4 @@
 export const PERSON_CONTEXT_CONTRACT_VERSION = 'kentaurai-person-context-v1';
 export const PERSON_CONTEXT_FEATURE_VERSION = 'person_context_v1';
+export const PERSON_CONTEXT_WINDOWS_DAYS = Object.freeze([14, 30, 90, 365]);
+export const PERSON_CONTEXT_BASELINE_DAYS = 365;
