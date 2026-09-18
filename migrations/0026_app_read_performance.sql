49995a303d4ca38edde3633d4634060173917df7
mstpln <mattias.bjerstedt@gmail.com>
2026-09-18 21:34:52 +0200

    Keep migration 0026 limited to genuinely new hot-path indexes

--- migrations/0026_app_read_performance.sql
+++ migrations/0026_app_read_performance.sql
@@ -3,20 +3,8 @@ PRAGMA foreign_keys = ON;
 CREATE INDEX IF NOT EXISTS idx_races_track_date
   ON races(track_id, race_date);
 
-CREATE INDEX IF NOT EXISTS idx_entries_trainer
-  ON race_entries(trainer_id);
-
-CREATE INDEX IF NOT EXISTS idx_entries_driver
-  ON race_entries(driver_id);
-
 CREATE INDEX IF NOT EXISTS idx_entries_horse_race
   ON race_entries(horse_id, race_id);
-
-CREATE INDEX IF NOT EXISTS idx_entries_trainer_race
-  ON race_entries(trainer_id, race_id);
-
-CREATE INDEX IF NOT EXISTS idx_entries_driver_race
-  ON race_entries(driver_id, race_id);
 
 CREATE INDEX IF NOT EXISTS idx_game_legs_race
   ON game_legs(race_id);
