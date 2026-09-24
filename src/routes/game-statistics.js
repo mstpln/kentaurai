dad5725ad08eb0f09141f2f11a577a37311b6774
mstpln <mattias.bjerstedt@gmail.com>
2026-09-24 14:04:36 +0200

    Use chronological earliest statistics race cutoff

--- src/routes/game-statistics.js
+++ src/routes/game-statistics.js
@@ -14,10 +14,12 @@ const PRE_RACE_CUTOFF = `(
     SELECT gr.bet_stop_at AS value
     UNION ALL SELECT gr.scheduled_start_at
     UNION ALL SELECT (
-      SELECT MIN(r0.scheduled_start_at)
+      SELECT r0.scheduled_start_at
       FROM game_legs gl0
       JOIN races r0 ON r0.id=gl0.race_id
-      WHERE gl0.game_round_id=gr.id
+      WHERE gl0.game_round_id=gr.id AND r0.scheduled_start_at IS NOT NULL
+      ORDER BY julianday(r0.scheduled_start_at) ASC
+      LIMIT 1
     )
   ) WHERE value IS NOT NULL ORDER BY julianday(value) ASC LIMIT 1
 )`;
